2927027884d9b8f73ad1380e2bf69e52ea8fe8d3
Matan Narkiss <dv.narxx@gmail.com>
2021-03-07 11:57:26 +0200

    no share

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -119,8 +119,14 @@
                 <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
                 <v-card-actions>
                   <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                  <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                  <v-btn color="blue darken-1"
+                         text
+                         @click="closeDelete"
+                  >Cancel</v-btn>
+                  <v-btn color="blue darken-1"
+                         text
+                         @click="deleteItemConfirm"
+                  >OK</v-btn>
                   <v-spacer></v-spacer>
                 </v-card-actions>
               </v-card>
@@ -129,7 +135,8 @@
         </template>
 
         <template v-slot:[`item.share`]="{ item }">
-          <a :href="getShareLink(item)">Share Link</a>
+          <a :href="getShareLink(item)" v-if="item.shareSlug">Share Link</a>
+          <span v-else>No share</span>
         </template>
 
         <template v-slot:[`item.actions`]="{ item }">
